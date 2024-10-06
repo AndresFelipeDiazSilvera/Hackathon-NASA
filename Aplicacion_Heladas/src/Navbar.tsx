4c58960ffd58f10627246c726b749474a56b37e0
santiagohm <hermolsan@gmail.com>
2024-10-06 15:19:35 -0500

    locations

--- Aplicacion_Heladas/src/Navbar.tsx
+++ Aplicacion_Heladas/src/Navbar.tsx
@@ -4,15 +4,15 @@ function Navbar() {
   return (
     <nav className="bg-white border-gray-200 dark:bg-[#1A5319]">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a
-          href=""
+        <Link
+          to="/"
           className="flex items-center space-x-3 rtl:space-x-reverse"
         >
           <img src="/public/logo.png" className="h-8" alt="Logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             FreezeWatch
           </span>
-        </a>
+        </Link>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
           <Link to="/form">
             <button
